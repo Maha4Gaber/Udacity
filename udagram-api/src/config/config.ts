e9f5640a0d62976fbe87a276447eb3aa790f1d08
Maha4Gaber <Maha.20353596@compit.aun.edu.eg>
2025-08-27 14:17:35 +0300

    TE

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,15 +1,21 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
+
+// ENV variables
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
+// Are Also needed
 
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  port: Number(process.env.POSTGRES_PORT),
   host: process.env.POSTGRES_HOST,
-  dialect: "postgres",
-  aws_region: process.env.AWS_DEFAULT_REGION,
-  aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
+  aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  dbPort: Number(process.env.DB_PORT),
+  port: Number(process.env.PORT),
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,18 @@
-import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-  port: config.port,
-
-  dialect: "postgres",
-  storage: ":memory:",
+  port: config.dbPort,
+  dialect: 'postgres',
+  storage: ':memory:',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Only use this for testing
+    },
+  },
 });
